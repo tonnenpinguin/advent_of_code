247744037f68199532825e65d96d92eebc733bc6
Bernhard Hackl <bernhard.hackl@sportalliance.com>
2022-12-06 21:39:07 +0100

    Add script for the lazy

---
+++ add_day.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage $0 dayNr"
+    exit -1
+fi
+
+day=$1
+year="twenty_two"
+yearAtom="TwentyTwo"
+
+
+touch "lib/$year/inputs/day$day.txt"
+cat << EOF > "lib/$year/day$day.ex"
+defmodule Aoc.$yearAtom.Day$day do
+  @input File.read!(Path.join(__DIR__, "inputs/day$day.txt"))
+
+  @doc """
+  ## Examples
+
+    iex> #{__MODULE__}.part1(~s(1000\\\\n2000\\\\n3000))
+    24000
+
+  """
+  def part1(input \\\\ @input) do
+    input
+  end
+end
+EOF
+
+cat << EOF > "test/$year/day${day}_test.exs"
+defmodule Aoc.$yearAtom.Day${day}Test do
+  use ExUnit.Case
+  doctest Aoc.$yearAtom.Day$day
+end
+EOF
