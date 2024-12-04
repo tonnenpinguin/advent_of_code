ec0477b22728a0f6ee6dba3585008f90494149f9
Bernhard Hackl <bernhard.hackl@tonnenpinguin.solutions>
2024-12-04 14:40:42 +0100

    make session optional

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,15 @@
 import Config
 
+session =
+  case File.read("./.session") do
+    {:ok, session} -> session
+    _ -> nil
+  end
+
 config :advent_of_code_utils,
   gen_tests?: true,
   auto_compile?: true,
   time_calls?: true,
-  session: File.read!("./.session")
+  session: session
 
 config :iex, inspect: [charlists: :as_lists]
